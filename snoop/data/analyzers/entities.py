e3f5b6d89124d642fed941d9974ee3735ca46d12
MattesR <mattes.ruckdeschel@tuhh.de>
2021-02-15 14:07:35 +0100

    add entity extraction analyzer

--- snoop/data/analyzers/entities.py
+++ snoop/data/analyzers/entities.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from urllib.parse import urljoin
+from .. import models
 import requests
 
 # TODO: put NLP_SERVICE_URL into snoop settings
@@ -27,3 +29,37 @@ def get_language(text):
     resp = call_nlp_server('language_detection', data)
     language = resp.json()['language']
     return language
+
+
+# Create all db entries which are related to that entity.
+def create_db_entries(entity, model, language, text_source, digest):
+    db_ent = models.Entity.objects.get_or_create(entity=entity['text'], type=entity['type'])
+    models.EntityHit.objects.get_or_create(entity=db_ent,
+                                           model=models.LanguageModel.objects.get(
+                                               language=language, method=model),
+                                           text_source=text_source,
+                                           start=entity['start'],
+                                           end=entity['end'],
+                                           digest=digest
+                                           )
+
+
+def extract_enitities(text, text_source, digest):
+    # TODO: Add entity Extraction setting to snoop settings
+    nlp_response = get_entities(text)
+    ents = nlp_response['entities']
+    results = {}
+    for entity in ents:
+        create_db_entries(entity, nlp_response['model'],
+                          nlp_response['language'], text_source, digest)
+    if settings.DETECT_LANGUAGE:
+        results['lang'] = nlp_response['language']
+        results['entities'] = [(k['text'], k['label']) for k in ents]
+        unique_ents = set([(k['text'], k['type']) for k in ents])
+        results['entity_ids'] = [models.Entity.objects.get(entity=k[0], type=k[1]).id
+                                 for k in unique_ents]
+
+    if settings.DETECT_LANGUAGE and 'lang' not in results:
+        text = text[:2500]
+        results['lang'] = get_language(text[:2500])
+    return results
